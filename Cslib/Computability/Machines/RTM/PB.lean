562b0e23aed9f411a840d3b48d994dc73e3db438
crei <crei.github@gmail.com>
2026-06-17 18:13:59 +0200

    lint

--- Cslib/Computability/Machines/RTM/PB.lean
+++ Cslib/Computability/Machines/RTM/PB.lean
@@ -427,9 +427,9 @@ def UsesLinearTimeAndSpace (impl : PB) : Prop :=
 def ComputesFunInTimeAndSpace {α β : Type} [DataEncode α] [DataEncode β]
     (p : PB → PB) (φ : α → β) (t s : α → ℕ) : Prop :=
   ∀ (env : List Value) (a : PB) (x : α) (ta sa : ℕ),
-    (∀ ext : List Value, ProgSem (env ++ ext) (a (env.length + ext.length))
+    (∀ ext, ProgSem (env ++ ext) (a (env.length + ext.length))
       (.data (DataEncode.encode x)) ta sa) →
-    (∀ ext : List Value, ∃ t' ≤ t x, ∃ s' ≤ s x, ProgSem (env ++ ext) (p a (env.length + ext.length))
+    (∀ ext, ∃ t' ≤ t x, ∃ s' ≤ s x, ProgSem (env ++ ext) (p a (env.length + ext.length))
       (.data (DataEncode.encode (φ x))) (t' + ta) (max s' sa))
 
 def ComputesFunInLinearTimeAndSpace {α β : Type} [DataEncode α] [DataEncode β]
